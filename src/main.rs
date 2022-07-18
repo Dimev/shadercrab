72039eddd2be03eb408f6b8441542b261d808a9b
Dimas Leenman <skythedragon@outlook.com>
2022-07-18 13:56:21 +0200

    wgpu rewrite part 1: config file loading

--- src/main.rs
+++ src/main.rs
@@ -1,223 +1,31 @@
-pub mod buffer;
-pub mod drawer;
-pub mod parser;
-pub mod program;
+use clap::Parser;
+use parse::Shadertoy;
+use std::path::*;
 
-use glium::glutin;
-use glutin::event::{ElementState, Event, MouseButton, VirtualKeyCode, WindowEvent};
+mod parse;
 
-use crate::drawer::*;
-use crate::parser::*;
-use crate::program::*;
+/// Shadercrab, a dektop shadertoy emulator
+#[derive(Debug, Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Arguments {
+    /// what to multiply the resolution by when rendering
+    #[clap(short, long, value_parser, default_value_t = 1.0)]
+    scale: f32,
+
+    /// file path to the config file or shader
+    #[clap(value_parser)]
+    config: PathBuf,
+
+    /// treat the file as a shader
+    #[clap(long, parse(from_flag))]
+    shader: bool,
+}
 
 fn main() {
-    // figure out what shader to load
-    let (file_path, render_scale): (String, f32) = match &std::env::args().collect::<Vec<String>>()
-        [..]
-    {
-        [_, x] => (x.clone(), 1.0),
-        [_, x, p, y] if p == "-s" || p == "--scale" => (
-            x.clone(),
-            y.parse::<f32>().map_or_else(
-                |_| {
-                    println!("Could not parse scale as a float");
-                    std::process::exit(0);
-                },
-                |x| x,
-            ),
-        ),
-        _ => {
-            // no valid arguments, show the help menu
-            println!("Shadercrab {}", env!("CARGO_PKG_VERSION"));
-            println!("A simple shadertoy emulator");
-            println!("Usage:");
-            println!("shadercrab [path] [-s|--scale render scale]");
-            println!("	path: path to the shader file to use");
-            println!("  render scale: what resolution to render at compared to window resolution");
-            println!("");
-            println!("This opens a window that shows the shader");
-            println!("The shader is reloaded when the file is modified, or the r key is pressed");
-            println!("Any shader errors are printed to the terminal");
-            println!("");
-            println!("Shader format:");
-            println!("Shaders are in glsl, and need the function");
-            println!("	mainImage(out vec4 fragColor, in vec2 fragCoord)");
-            println!("where");
-            println!("	fragColor: output color for the pixel, in sRGB color space");
-            println!("	fragCoord: the pixel coordinate, with bottom left at (0, 0) and top right at (width, height)");
-            println!("	           width and height are the width and height of the window");
-            println!("");
-            println!("The following constants are also defined:");
-            println!("	float iTime: seconds since the shader was loaded");
-            println!("	int iFrame: current frame number");
-            println!("	vec3 iResolution: width, height and aspect ratio (y / x) of the window");
-            println!(
-                "	vec4 iMouse: xy: mouse position, changed when dragging with the left mouse button"
-            );
-            println!("	             zw: mouse button states (0 is up, 1 is down)");
-            return;
-        }
-    };
+    // get the args
+    let args = Arguments::parse();
 
-    // time at which the file got modified
-    // if it's not a valid file, show why we crashed
-    let mut time_stamp = {
-        match std::fs::metadata(&file_path) {
-            Ok(x) => x
-                .modified()
-                .expect("Failed to access file access timestamp"),
-            Err(reason) => {
-                println!("Failed to open file: {:?}", reason);
-                return;
-            }
-        }
-    };
+    let toy = Shadertoy::new(&args.config, args.shader);
 
-    // start up the event loop
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new()
-        .with_title(format!(
-            "Shadercrab {} - {}",
-            env!("CARGO_PKG_VERSION"),
-            file_path
-        ))
-        .with_inner_size(glutin::dpi::PhysicalSize::new(800, 450));
-    let cb = glutin::ContextBuilder::new().with_vsync(true);
-    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
-
-    println!(
-        "GPU: {}\nVendor: {}\nOpenGL version: {}",
-        display.get_opengl_renderer_string(),
-        display.get_opengl_vendor_string(),
-        display.get_opengl_version_string()
-    );
-
-    // current screen size
-    let resolution = display.get_framebuffer_dimensions();
-
-    // the actual drawing manager
-    let mut drawer = Drawer::new(&display, resolution.0, resolution.1, render_scale);
-
-    // load the program
-    // mutable so we can reload later
-    //let program = load_program(&display, &file_path);
-    let shadertoy = Shadertoy::new(&std::path::PathBuf::from(&file_path));
-
-    // apply it
-    shadertoy.load_shaders(&display, &mut drawer);
-
-    // time since program start
-    let mut start_time = std::time::Instant::now();
-
-    // current frame
-    let mut frame = 0;
-
-    // mouse position and status
-    let mut mouse_pos = (0, 0);
-    let mut mouse_stat = (false, false);
-
-    // whether we have focus
-    let mut focus = false;
-
-    // draw once to show it
-    drawer.draw(&display, 0.0, frame, mouse_pos, mouse_stat, render_scale);
-
-    // and run the event loop
-    event_loop.run(move |event, _, control_flow| {
-        // TODO reload program if the file changed
-        // close if needed
-        match event {
-            Event::WindowEvent { event, .. } => match event {
-                // close if requested
-                WindowEvent::CloseRequested => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit
-                }
-                // resized
-                WindowEvent::Resized(s) => {
-                    // rename the window to include the size
-                    display.gl_window().window().set_title(&format!(
-                        "Shadercrab {} - {}x{} - {}",
-                        env!("CARGO_PKG_VERSION"),
-                        (s.width as f32 * render_scale) as u32,
-                        (s.height as f32 * render_scale) as u32,
-                        file_path
-                    ));
-                }
-                // check focus
-                WindowEvent::Focused(f) => focus = f,
-                // check mouse position
-                WindowEvent::CursorMoved { position, .. } => {
-                    mouse_pos = if mouse_stat.0 {
-                        (position.x as u32, position.y as u32)
-                    } else {
-                        mouse_pos
-                    }
-                }
-                // check mouse buttons
-                WindowEvent::MouseInput {
-                    state,
-                    button: MouseButton::Left,
-                    ..
-                } => mouse_stat.0 = state == ElementState::Pressed,
-                WindowEvent::MouseInput {
-                    state,
-                    button: MouseButton::Right,
-                    ..
-                } => mouse_stat.1 = state == ElementState::Pressed,
-                // check if we need to reload
-                WindowEvent::KeyboardInput { input, .. } => {
-                    if input.virtual_keycode == Some(VirtualKeyCode::R)
-                        && input.state == ElementState::Released
-                        && focus
-                    {
-                        println!("Reloaded shader");
-                        //drawer.buffers[0].program = load_program(&display, &file_path);
-                        // reset the time as well
-                        start_time = std::time::Instant::now();
-                        // reset the frame
-                        frame = 0;
-                        // reset the mouse
-                        mouse_pos = (0, 0);
-                    }
-                }
-                _ => (),
-            },
-            Event::NewEvents(glutin::event::StartCause::ResumeTimeReached { .. })
-            | Event::NewEvents(glutin::event::StartCause::Init) => {
-                // check if the shader was edited
-                /*let new_time_stamp = std::fs::metadata(&file_path).unwrap().modified().unwrap();
-                if new_time_stamp != time_stamp {
-                    // reload if it was
-                    println!("Reloaded shader");
-                    drawer.buffers[0].program = load_program(&display, &file_path);
-                    // reset the time as well
-                    start_time = std::time::Instant::now();
-                    // reset the frame
-                    frame = 0;
-                    // reset the mouse
-                    mouse_pos = (0, 0);
-                    // and prevent continuous reloading
-                    time_stamp = new_time_stamp;
-                }*/
-
-                // increment the frame
-                frame += 1;
-
-                // we're reached the end of the frame, redraw
-                drawer.draw(
-                    &display,
-                    start_time.elapsed().as_secs_f32(),
-                    frame,
-                    mouse_pos,
-                    mouse_stat,
-                    render_scale,
-                );
-                // and request a redraw, at 60 fps
-                *control_flow = glutin::event_loop::ControlFlow::WaitUntil(
-                    std::time::Instant::now() + std::time::Duration::from_secs_f32(1.0 / 60.0),
-                );
-            }
-            _ => (),
-        }
-    });
+    println!("{:?}", toy);
 }
---
+++ src/parse.rs
@@ -0,0 +1,212 @@
+use image::Rgba32FImage;
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::path::*;
+use std::time::SystemTime;
+
+/// helper function to check when a path changed
+fn file_changed_time(path: &Path) -> Result<SystemTime, String> {
+    std::fs::metadata(path)
+        .map_err(|x| format!("File {} not found: {}", path.to_string_lossy(), x))?
+        .modified()
+        .map_err(|x| {
+            format!(
+                "Could not get modified timestamp of file {}: {}",
+                path.to_string_lossy(),
+                x
+            )
+        })
+}
+
+/// struct storing a single shader's config
+#[derive(Debug, Deserialize)]
+pub struct Channel {
+    /// path to the shader file
+    pub shader: Option<PathBuf>,
+
+    /// path to the image file
+    pub image: Option<PathBuf>,
+
+    /// list of inputs
+    pub inputs: Option<HashMap<String, String>>,
+}
+
+/// struct for a full shader definition
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    /// shader main input/output
+    pub output: String,
+
+    /// shader common
+    pub common: Option<PathBuf>,
+
+    /// shader defines
+    pub channels: HashMap<String, Channel>,
+}
+
+/// single shader channel
+#[derive(Debug)]
+pub enum ShaderChannel {
+    Shader {
+        shader: String,
+        inputs: HashMap<String, String>,
+    },
+    Image {
+        image: Rgba32FImage,
+    },
+}
+
+/// full shader descriptor
+#[derive(Debug, Default)]
+pub struct Shadertoy {
+    /// all files to watch
+    files: Vec<(PathBuf, SystemTime)>,
+
+    /// config file itself
+    config_file: PathBuf,
+
+    /// which shader to show to the screen
+    main_shader: String,
+
+    /// whether this is a shader, or a full config
+    is_shader: bool,
+
+    /// common shader
+    common: String,
+
+    /// all channels
+    channels: HashMap<String, ShaderChannel>,
+}
+
+impl Shadertoy {
+    /// check if it needs to be reloaded
+    pub fn check_reload(&self) -> bool {
+        self.files.iter().any(|(file_path, time_changed)| {
+            if let Ok(time) = file_changed_time(file_path) {
+                // only reload if the changed time is newer than the old time
+                time > *time_changed
+            } else {
+                // we need to reload if the file stopped existing
+                true
+            }
+        })
+    }
+
+    /// make a new shadertoy, from the given config file path
+    pub fn new(path: &Path, is_shader: bool) -> Result<Self, String> {
+        if is_shader {
+            // see when the file changed
+            let file_changed = file_changed_time(path)?;
+
+            // load the shader file
+            let shader = std::fs::read_to_string(path)
+                .map_err(|x| format!("File {} not found: {}", path.to_string_lossy(), x))?;
+
+            // make the hashmap
+            let mut channels = HashMap::new();
+            channels.insert(
+                "main".into(),
+                ShaderChannel::Shader {
+                    shader,
+                    inputs: HashMap::new(),
+                },
+            );
+
+            Ok(Self {
+                files: vec![(path.into(), file_changed)],
+                config_file: path.into(),
+                main_shader: "main".into(),
+                common: "".into(),
+                is_shader,
+                channels,
+            })
+        } else {
+            // parse the file
+            let config: Config = toml::from_str(
+                &std::fs::read_to_string(path)
+                    .map_err(|x| format!("File {} not found: {}", path.to_string_lossy(), x))?,
+            )
+            .map_err(|x| format!("Failed to parse {}: {}", path.to_string_lossy(), x))?;
+
+            // get the full path of the directory
+            let directory = path
+                .parent()
+                .ok_or(format!(
+                    "Failed to get parent directory of {}",
+                    path.to_string_lossy()
+                ))?
+                .canonicalize()
+                .map_err(|x| {
+                    format!(
+                        "Failed to get full path of {}: {}",
+                        path.to_string_lossy(),
+                        x
+                    )
+                })?;
+
+            // try and load the common shader
+            let common = if let Some(common) = &config.common {
+                std::fs::read_to_string(if common.is_abosulte() { common } else { directory.join(common) })
+                    .map_err(|x| format!("File {} not found: {}", (if common.is_abosulte() { common } else { directory.join(common) }).to_string_lossy(), x))?
+            } else {
+                String::new()
+            };
+
+            // setup without looping
+            let mut toy = Self {
+                files: vec![(path.into(), file_changed_time(path)?)],
+                config_file: path.into(),
+                common,
+                ..Default::default()
+            };
+
+            // go over all channels in the shader, and convert them
+            for (name, channel) in config.channels.into_iter() {
+                let shader_channel = match (channel.shader, channel.image) {
+                    (Some(shader_file), None) => {
+                        // load the shader, and it's file
+                        let shader_path = if shader_file.is_absolute() { shader_file} else { directory.join(shader_file) };
+                        let shader = std::fs::read_to_string(&shader_path).map_err(|x| {
+                            format!("Failed to load {}: {}", shader_path.to_string_lossy(), x)
+                        })?;
+
+                        toy.files
+                            .push((shader_path.clone(), file_changed_time(&shader_path)?));
+
+                        // then construct the channel
+                        ShaderChannel::Shader {
+                            shader,
+                            inputs: channel.inputs.unwrap_or(HashMap::new()),
+                        }
+                    }
+                    (None, Some(image_file)) => {
+                        // load the shader, and it's file
+                        let image_path = if image_file.is_absolute() { image_file) else { directory.join(image_file) };
+                        let image = image::open(&image_path).map_err(|x| {
+                            format!("Failed to load {}: {}", image_path.to_string_lossy(), x)
+                        })?;
+
+                        toy.files
+                            .push((image_path.clone(), file_changed_time(&image_path)?));
+
+                        // then construct the channel
+                        ShaderChannel::Image {
+                            image: image.to_rgba32f(),
+                        }
+                    }
+                    _ => {
+                        return Err(format!(
+                            "Channel {} had both a shader and image, which isn't allowed",
+                            name
+                        ))
+                    }
+                };
+
+                // add it to the toy
+                toy.channels.insert(name, shader_channel);
+            }
+
+            Ok(toy)
+        }
+    }
+}
